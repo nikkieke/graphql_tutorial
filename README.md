941a25892918477c5281036d95ee29a8473620cd
nikkieke <95222620+nikkieke@users.noreply.github.com>
2024-01-02 17:16:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# graphql_tutorial
+
+A simple food app to practice Graphql api implementation
+
+## Key Features
+- Google Signup/Signin
+- Get list of all products available
+- Update user profile
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
